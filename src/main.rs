afc24d6bc8ede81dffc5fe6f772401ad03fb1ac1
Heverton <hb.hevertonborges@gmail.com>
2025-02-25 18:15:32 -0300

    axum hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let router: Router = Router::new().route("/", get("Hello world!"));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, router).await.unwrap();
 }
