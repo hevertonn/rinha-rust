915f2421842b04548c30e53edb260a34da0f606a
Heverton <hb.hevertonborges@gmail.com>
2025-02-25 19:02:16 -0300

    environment variables

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,24 @@ use axum::{
     Router,
 };
 
+use dotenvy::dotenv;
+use std::env;
+
 #[tokio::main]
 async fn main() {
+    dotenv().expect("Failed to load environment variables!");
+
+    let port = env::var("PORT").unwrap_or(String::from("8080"));
+
     let router: Router = Router::new()
         .route("/pessoas", post("post pessoas"))
         .route("/pessoas/", get("get pessoas id"))
         .route("/pessoas", get("get pessoas query"))
         .route("/contagem-pessoas", get("get contagem-pessoas"));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await
+        .unwrap();
 
     axum::serve(listener, router).await.unwrap();
 }
