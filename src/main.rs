e9839da20f634fd0d19fb4289701d63a8441f94d
Heverton <hb.hevertonborges@gmail.com>
2025-02-25 18:23:33 -0300

    routes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
-    let router: Router = Router::new().route("/", get("Hello world!"));
+    let router: Router = Router::new()
+        .route("/pessoas", post("post pessoas"))
+        .route("/pessoas/", get("get pessoas id"))
+        .route("/pessoas", get("get pessoas query"))
+        .route("/contagem-pessoas", get("get contagem-pessoas"));
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, router).await.unwrap();
