51cc3c2ccac599417477f6a26412f8fcc680d4ce
Heverton <hb.hevertonborges@gmail.com>
2025-02-25 20:20:05 -0300

    setup postgres connection

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,26 @@ use axum::{
     routing::{get, post},
     Router,
 };
-
 use dotenvy::dotenv;
 use std::env;
+use tokio_postgres::NoTls;
 
 #[tokio::main]
 async fn main() {
-    dotenv().expect("Failed to load environment variables!");
+    dotenv().expect("Failed to load environment variables");
 
-    let port = env::var("PORT").unwrap_or(String::from("8080"));
+    let port = env::var("PORT").unwrap_or(String::from("3000"));
+    let db_config = env::var("DB_CONFIG").expect("Postgres settings aren't in the environment");
+
+    let (client, connection) = tokio_postgres::connect(&db_config, NoTls)
+        .await
+        .expect("Failed to connect to postgres");
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Connection error: {e}");
+        }
+    });
 
     let router: Router = Router::new()
         .route("/pessoas", post("post pessoas"))
